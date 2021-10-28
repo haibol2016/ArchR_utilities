e0ff56f3475f77fa168187b9999787b9287e8ee0
Haibo Liu <48487183+haibol2016@users.noreply.github.com>
2021-10-28 14:33:41 -0400

    allow gz output

--- add_gene2gtf.R
+++ add_gene2gtf.R
@@ -1,7 +1,7 @@
 library("rtracklayer")
 
 ## Amend UCSC GTF files by adding gene entries
-add_gene2gtf <- function(gtf, out_dir = ".")
+add_gene2gtf <- function(gtf, out_dir = ".", gz_out = TRUE)
 {
     if (!file.exists(gtf)){
         stop("gtf doesn't exist!")
@@ -13,10 +13,9 @@ add_gene2gtf <- function(gtf, out_dir = ".")
     
     ## split into GRangesList, suppose all gene_ids are unique
     gtf_GRL <- split(gtf_GR, mcols(gtf_GR)$gene_id)
-    fixed_gtf_GRL <- endoapply(gtf_GRL, function(.x){
+    fixed_gtf_GRL <- endoapply(gtf_GRL[1:10], function(.x){
         gene <- reduce(.x, ignore.strand=FALSE)
         tx_GR <- .x[.x$type == "transcript"][1]
-        mcols(gene) <- mcols(tx_GR)
         gene$type <- "gene" 
         gene$source <- tx_GR$source
         gene$gene_id <- tx_GR$gene_id
@@ -24,9 +23,21 @@ add_gene2gtf <- function(gtf, out_dir = ".")
         append(gene, .x)
     })
     fixed_gtf <- unlist(fixed_gtf_GRL)
-    gtf_file <- file.path(out_dir, 
-                          paste0("amended.",Sys.Date(),".", basename(gtf)))
+    out_filename <- paste0("amended.", Sys.Date(),".", 
+                           gsub(".(gz|bz2|xz)$", "", basename(gtf)))
+    if (gz_out)
+    {
+        out_filename <- file.path(out_dir, 
+                                  paste0(out_filename, ".gz"))
+        gtf_file <- gzfile(out_filename, open = "w")
+    } else {
+        out_filename <- file.path(out_dir, 
+                                  out_filename)
+        gtf_file <- file(out_filename, open = "w")
+    }
+
     rtracklayer::export(fixed_gtf, 
                         con = gtf_file)
-    gtf_file
+    close(gtf_file)
+    out_filename
 }
