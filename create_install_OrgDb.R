a52f6317700fb1d4432aa950c13694052dc13e33
Haibo Liu <48487183+haibol2016@users.noreply.github.com>
2021-10-29 10:12:46 -0400

    fix bugs

--- create_install_OrgDb.R
+++ create_install_OrgDb.R
@@ -1,33 +1,33 @@
 library("AnnotationForge")
-create_install_OrgDb <- function(geneID_symbom_df, 
+create_install_OrgDb <- function(geneID_symbol_df, 
                          tax_id = "9606", 
                          genus = "Homo",
                          species = "sapeins",
                          package_dest_dir = "./")
 {
-    if (!is.data.frame(geneID_symbom_df) || 
-        any(colnames(geneID_symbom_df) != c("gene_id", "symbol")))
+    if (!is.data.frame(geneID_symbol_df) || 
+        any(colnames(geneID_symbol_df) != c("gene_id", "symbol")))
     {
-        stop(geneID_symbom_df, " is not a dataframe or the colnames are not
+        stop("geneID_symbol_df is not a dataframe or the colnames are not
              correct!")
     }
-    id_type <- {if (all(grepl("^ENS.*?G\\d+", geneID_symbom_df[, 1], perl = TRUE)))
+    id_type <- {if (all(grepl("^ENS.*?G\\d+", geneID_symbol_df[, 1], perl = TRUE)))
     {"ENAEMBL"} else if (all(grepl("^\\d+$", 
-                                       geneID_symbom_df[, 1], perl = TRUE)))
+                                       geneID_symbol_df[, 1], perl = TRUE)))
     {"GENEID"} else {"unkown"}}
     
     if (id_type == "unkown"){
         stop("gene_id should be either Ensembl gene ID or Entrez Gene ID!")
     }
     
-    fSym <- unique(geneID_symbom_df[, c(1,3)])
+    fSym <- unique(geneID_symbol_df[, c(1,3)])
     colnames(fSym) <- c("GID", "SYMBOL")
     if (all(grepl("_", fSym$SYMBOL, perl= TRUE)))
     {
-          fSym$SYMBOL <- gsub("_.+", "", fSym$SYMBOL, perl = TRUE)
+          fSym$SYMBOL <- gsub("_.+$", "", fSym$SYMBOL, perl = TRUE)
     }
     
-    ensembl <- unique(geneID_symbom_df[, c(1,1)])
+    ensembl <- unique(geneID_symbol_df[, c(1,1)])
     colnames(ensembl) <- {if (id_type == "ENSEMBL") {c("GID", "ENSEMBL")} else {
         c("GID", "GENEID")
     }}
@@ -37,7 +37,6 @@ create_install_OrgDb <- function(geneID_symbom_df,
     }
     
     makeOrgPackage(gene_info = fSym, 
-                   ensembl_trans = ensembl_trans,
                    ensembl = ensembl,
                    version = "0.1",
                    maintainer = "Some One <so@someplace.org>",
@@ -48,9 +47,9 @@ create_install_OrgDb <- function(geneID_symbom_df,
                    species= species,
                    goTable=NULL)
     
-    package_name <- paste("org", paste0(gsub("^(.).+", "\\1", genus, perl = TRUE),
+    package_name <- paste("org", paste0(gsub("^(.).*", "\\1", genus, perl = TRUE),
                            tolower(species)), "eg.db", sep = ".")
     
-    install.packages(file.path(tmpdir, package_name), 
+    install.packages(file.path(package_dest_dir, package_name), 
                      type = "source", repos=NULL)
 }
